5b9157cf6740f371fde10166dc6153413c8a4114
Lindsay Wardell <three060@gmail.com>
2023-10-09 09:27:38 -0700

    Add PWA

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Dingy Bell
+# Witchy Bell
 
 This is a simple app to be used in meditations or other activities with some sort of cadence. A bell will ring at a given interval, with a timer running to inform how much longer until the next chime.
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Witchy Bell",
+        short_name: "Witchy Bell",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#242424",
+        description: "A simple bell for witches",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
